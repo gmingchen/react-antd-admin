c38c10e4191b12c9afd4987e48e2967a2c58062f
gumingchen <1240235512@qq.com>
2022-10-24 16:48:37 +0800

    add axios

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { lazy } from 'react'
+import { Navigate} from 'react-router-dom'
 
 const lazyLoad = (path) => {
   const Element = lazy(() => import(`@/views${path}`))
@@ -9,7 +10,8 @@ const constant = [
   { path: '/login', element: lazyLoad('/constant/login') },
   { path: '/401', element: lazyLoad('/constant/401') },
   { path: '/404', element: lazyLoad('/constant/404') },
-  { path: '/500', element: lazyLoad('/constant/500') }
+  { path: '/500', element: lazyLoad('/constant/500') },
+  { path: '/', element: <Navigate to='/login' replace /> }
 ]
 
 const main = {
