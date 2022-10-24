fd7699d13523d9582e98cc1cbb95e45ab7cb2fd9
gumingchen <1240235512@qq.com>
2022-10-24 13:36:19 +0800

    添加 antd sass

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './assets/sass/index.scss'
+import { ConfigProvider, Button } from 'antd'
+
+export default function App() {
+  return (
+    <>
+      <Button>App....</Button>
+    </>
+  );
+}
